64002683b8547ed0a0641ab3a02acd80dd31c056
Andrzej <szkudlarek.andrzej@gmail.com>
2016-02-25 13:34:16 +0100

    init commit

---
+++ .i3/scripts/lock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+icon="/home/leto/.i3/scripts/icon.png"
+tmpbg='/tmp/screen.png'
+
+(( $# )) && { icon=$1; }
+
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+i3lock -u -i "$tmpbg"
