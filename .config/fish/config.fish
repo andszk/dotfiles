b309c434931a95f37993cac8ed71e729d37aa7a5
Andrzej <szkudlarek.andrzej@gmail.com>
2016-02-26 21:32:46 +0100

    fish + terminator

---
+++ .config/fish/config.fish
@@ -0,0 +1,4 @@
+alias netflix='google-chrome https://www.netflix.com'
+alias duelyst='google-chrome http://beta.duelyst.com'
+alias update='sudo apt-get update && sudo apt-get upgrade'
+alias ainstall='sudo apt-get install'
